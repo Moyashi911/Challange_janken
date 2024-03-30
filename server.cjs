7a87e212e13f2ee55da7f4edde9f8df6320b70f9
Moyashi911 <sabanomisoni911@gmail.com>
2024-03-30 23:53:10 +0900

    文字化け

--- server.cjs
+++ server.cjs
@@ -1,16 +1,8 @@
 // node.jsモジュールの読み込み
 const http = require('http');
 const fs = require('fs');
-const iconv = require("iconv-lite")
-
-//文字化け防止
-fs.readFile('path-to/shift-jis.txt', function(err, data){
-    if (err) throw err;
-    // console.log( data.toString('UTF-8') );    // (参考)UTF-8の場合はtoString()メソッドで文字列を取り出せる
-    var buf    = new Buffer(data, 'binary');     //バイナリバッファを一時的に作成する
-    var retStr = iconv.decode(buf, "Shift_JIS"); //作成したバッファを使い、iconv-liteでShift-jisからutf8に変換
-    console.log(retStr);
-  });
+const jschardet = require('jschardet');
+const Iconv = require('iconv').Iconv;
 // ファイルの読み込み
 const html = fs.readFileSync('index.html', 'utf8');
 const css = fs.readFileSync('style.css', 'utf8');
